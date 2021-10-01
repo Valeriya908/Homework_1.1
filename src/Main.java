35214a2b3764eb70405c7f7983a71bbf7036ee0e
Valeriya Polukhina <valeriapolyxina@mail.ru>
2021-10-01 14:22:55 +0300

    Typo fixed

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int currentBalance = 2_000_000_000;
         int moneyTransfer = 500_000_000;
-        int total = currentBalance * moneyTransfer;
+        int total = currentBalance + moneyTransfer;
         System.out.println(total);
     }
 }
